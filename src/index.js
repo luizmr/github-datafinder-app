ea871197fc94957d65010c3ff7833697f979f32b
luizmr <luizmmrocha@gmail.com>
2020-08-09 20:47:58 -0300

    auth update

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
 	<React.StrictMode>
 		<Auth0Provider
-			domain="dev-qzn9jrzs.us.auth0.com"
-			clientId="hS7NawKxktk3uEXD9h2c3JVXh0ZNoWSB"
+			domain="dev-13dja1ug.us.auth0.com"
+			clientId="DiBjLQdwCvzr7H7jThJCROlcua3XJzOT"
 			redirectUri={window.location.origin}
 			cacheLocation="localstorage"
 		>
